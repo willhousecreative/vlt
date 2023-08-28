2535fd8a11f76dfd91a798d1f2e787f98f77a5fc
willhousecreative <richard@willhousecreative.com>
2023-08-27 17:51:45 -0700

    SK and Accordion

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        observer.disconnect();
+        resolve(document.querySelector(selector));
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
