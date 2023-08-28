a75a9598a0f982e6556aae348b4eadbcb2e0cb0c
willhousecreative <richard@willhousecreative.com>
2023-08-27 20:36:23 -0700

    Custom JavaScript

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,17 @@
 // https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
 
+/********
+ * To use it:
+ *
+ * waitForElm('.some-class').then((elm) => {
+ *   console.log('Element is ready');
+ *   console.log(elm.textContent);
+ * });
+ * Or with async/await:
+ *
+ * const elm = await waitForElm('.some-class');
+ *
+ */
 function waitForElm(selector) {
   return new Promise((resolve) => {
     if (document.querySelector(selector)) {
