e5093d81fdddbbdfe0d6271fec6e17ef8e72001e
willhousecreative <richard@willhousecreative.com>
2023-11-20 21:22:49 -0800

    Read More Toggle Section Specific

--- js/footer-script-custom.js
+++ js/footer-script-custom.js
@@ -1,27 +1,99 @@
-const elContainer = document.querySelector(
-  'section[data-section-id="649da269c19d844c02d40cf4"] .list-item-content__description'
+const elContainer = document.querySelectorAll(
+  'section[data-section-id="65398b1efeed1d69b2a95eb2"] .list-item-content__description'
 );
-const el = document.querySelector(
-  'section[data-section-id="649da269c19d844c02d40cf4"] .list-item-content__description p'
+const el = document.querySelectorAll(
+  'section[data-section-id="65398b1efeed1d69b2a95eb2"] .list-item-content__description p'
 );
 console.log(el);
 
-// create a button element
-let buttonAnchor = document.createElement('a');
-buttonAnchor.innerText = 'Read More';
-buttonAnchor.classList.add('list-item-content__button');
-buttonAnchor.classList.add('sqs-block-button-element');
-buttonAnchor.classList.add('sqs-block-button-element--medium');
-buttonAnchor.classList.add('sqs-button-element--primary');
-buttonAnchor.title = 'Read More';
-buttonAnchor.href = '#';
-elContainer.appendChild(buttonAnchor);
+let buttonAnchor;
 
-// toggle classes and text on click
-buttonAnchor.addEventListener('click', (evt) => {
-  console.log('clicked');
-  txt = buttonAnchor.innerText;
-  buttonAnchor.innerText = txt == 'Read More' ? 'Read Less' : 'Read More';
-  el.classList.toggle('show');
-  el.classList.toggle('display-block');
+// create a button element
+
+elContainer.forEach((ele, index) => {
+  ele.id = 'item-' + index;
+
+  buttonAnchor = document.createElement('a');
+  buttonAnchor.innerText = 'Read More';
+  buttonAnchor.classList.add('read_more');
+  buttonAnchor.classList.add('list-item-content__button');
+  buttonAnchor.classList.add('sqs-block-button-element');
+  buttonAnchor.classList.add('sqs-block-button-element--medium');
+  buttonAnchor.classList.add('sqs-button-element--primary');
+  buttonAnchor.setAttribute('data-target', 'item-' + index);
+  buttonAnchor.title = 'Read More';
+  buttonAnchor.href = '#';
+  //   ele.innerHTML = 'Hello World';
+  ele.appendChild(buttonAnchor);
+
+  //   buttonAnchor.addEventListener('click', (evt) => {
+  //     console.log('clicked');
+  //     txt = buttonAnchor.innerText;
+  //     buttonAnchor.innerText = txt == 'Read More' ? 'Read Less' : 'Read More';
+  //     el.classList.toggle('show');
+  //     el.classList.toggle('display-block');
+  //   });
 });
+
+const buttons = document.getElementsByClassName('read_more');
+console.log(buttons);
+
+const onButtonClick = (event) => {
+  console.log('click');
+  const button = event.target;
+  const dataTarget = button.getAttribute('data-target');
+  const extraContent = document.getElementById(dataTarget);
+  button.innerHTML =
+    button.innerHTML === 'Show Less' ? 'Show More' : 'Show Less';
+  extraContent.classList.toggle('show');
+  extraContent.classList.toggle('display-block');
+};
+
+const attachingEvent = (e) => e.addEventListener('click', onButtonClick);
+
+Array.prototype.forEach.call(buttons, attachingEvent);
+// toggle classes and text on click
+
+/* From Squarspace
+ * https://forum.squarespace.com/topic/240607-read-more-read-less-accordion-toggle/
+ */
+// Add an event listener to run the code once the DOM content has been loaded
+// Select all elements with the 'list-item-content__button' class
+// var readToggleButtons = document.querySelectorAll('.list-item-content__button');
+// console.log(readToggleButtons);
+
+// // Check if the buttons exist on the page
+// if (readToggleButtons) {
+//   // Iterate through all the buttons
+//   readToggleButtons.forEach(function (readToggleButton) {
+//     // Add a click event listener to each button
+//     readToggleButton.addEventListener('click', function () {
+//       console.log('clicked');
+//       txt = buttonAnchor.innerText;
+//       buttonAnchor.innerText = txt == 'Read More' ? 'Read Less' : 'Read More';
+//       el.classList.toggle('show');
+//       el.classList.toggle('display-block');
+
+//       // Set a timeout to run the code after a short delay (50ms) to ensure the 'aria-expanded' attribute is updated before toggling the text
+//       //   setTimeout(function () {
+//       //     // Select the span element with the 'accordion-item__title' class inside the current button
+//       //     var readToggleSpan = readToggleButton.querySelector('#page article .accordion-item__title');
+//       //     // Determine if the current button is expanded based on the 'aria-expanded' attribute value
+//       //     var isExpanded = readToggleButton.getAttribute('aria-expanded') === 'true';
+//       //     // Toggle the text based on whether the button is expanded or not
+//       //     toggleText(readToggleSpan, isExpanded);
+//       //   }, 50);
+//     });
+//   });
+// }
+
+// Define the 'toggleText' function that changes the text of the given element based on the 'isExpanded' parameter
+//     function toggleText(element, isExpanded) {
+//       // If the button is expanded, set the text to 'Read less'
+//       if (isExpanded) {
+//         element.innerHTML = 'Read less';
+//       // Otherwise, set the text to 'Read more'
+//       } else {
+//         element.innerHTML = 'Read more';
+//       }
+//     }
