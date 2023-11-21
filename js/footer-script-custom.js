684b2c7c6eb10c141ca951fc623dac30572b58ee
willhousecreative <richard@willhousecreative.com>
2023-11-20 17:26:16 -0800

    Read More Toggle

---
+++ js/footer-script-custom.js
@@ -0,0 +1,23 @@
+const elContainer = document.querySelector('.list-item-content__description');
+const el = document.querySelector('.list-item-content__description p');
+console.log(el);
+
+// create a button element
+let buttonAnchor = document.createElement('a');
+buttonAnchor.innerText = 'Read More';
+buttonAnchor.classList.add('list-item-content__button');
+buttonAnchor.classList.add('sqs-block-button-element');
+buttonAnchor.classList.add('sqs-block-button-element--medium');
+buttonAnchor.classList.add('sqs-button-element--primary');
+buttonAnchor.title = 'Read More';
+buttonAnchor.href = '#';
+elContainer.appendChild(buttonAnchor);
+
+// toggle classes and text on click
+buttonAnchor.addEventListener('click', (evt) => {
+  console.log('clicked');
+  txt = buttonAnchor.innerText;
+  buttonAnchor.innerText = txt == 'Read More' ? 'Read Less' : 'Read More';
+  el.classList.toggle('show');
+  el.classList.toggle('display-block');
+});
